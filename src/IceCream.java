a7712846c1e18383a788e941abfce573afb5bb5e
paulm6438 <paulm6438@HRH-IC0027320.wrdsb.ca>
2015-12-03 11:02:04 -0500

    initial

--- src/IceCream.java
+++ src/IceCream.java
@@ -1,36 +1,30 @@
+
 /**
- * The IceCream class is a subclass of DessertItem. 
- * 
+ * The IceCream class is a subclass of DessertItem.
+ *
  * IceCream has a name, and a cost associated with it.
- * 
+ *
  * @author YOURNAMEHERE
  */
-public class IceCream extends DessertItem{
+public class IceCream extends DessertItem {
 
     private int icCost;
-    
-    public IceCream(String name, int cost)
-    {
+
+    public IceCream(String name, int cost) {
         super(name);
         this.icCost = cost;
     }
 
-  
     @Override
-    public String toString()
-    {
+    public String toString() {
         String output = "\n";
         output += "Name: " + super.getName() + "\n";
         return "";
     }
 
-    
-    
-    
     public int getCost() {
-        
+
         return this.icCost;
-        
+
     }
-    
 }
