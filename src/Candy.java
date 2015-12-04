f439f2c96454f7d4f3b1dcbf3b9664c421ba0ec2
alimu <alimu@192.168.0.17>
2015-12-04 17:01:00 -0500

    Merge origin/master
    Conflicts:
    	src/hhssadventure/Scene.java

--- src/Candy.java
+++ src/Candy.java
@@ -4,20 +4,57 @@
  * A Candy has a name, a weight (in pounds), and a price per pound. 
  * The weight and price per pound are used to calculate the cost of the Candy. 
  * 
- * @author YOURNAMEHERE
+ * @author Ali Murra
  */
-public class Candy {
+public class Candy extends DessertItem{
     
-
+    //declare private variables
+    private double weight;
+    private int pricePerLbs;
+    
+    /**
+     * 
+     * @param name of the candy
+     * @param weight of the candy
+     * @param pricePerLbs how much each pound costs
+     */
     public Candy(String name, double weight, int pricePerLbs)
     {
-
+        //send the name of the candy to DessertItems
+        super(name);
+        //make the private variables in the class the same as the actual cost and weight of the candys
+        this.weight = weight;
+        this.pricePerLbs = pricePerLbs;
     }
-
- 
     
+    /**
+     * 
+     * @return the name, cost and rates of the candy
+     */
     public String toString()
     {
-       return "";
+        //output the weight of the candy bought and the rate in dollars per pound
+        String output = this.weight + " lbs. @ $" + DessertShoppe.cents2dollarsAndCents(this.pricePerLbs) + " /lb." + "\n";
+        //output the name of candy
+        output+= this.getName();
+        //format to line up the price
+        int widthPreCost = DessertShoppe.RECEIPT_WIDTH - this.getName().length();
+        output += String.format("%" + widthPreCost + "s%n", DessertShoppe.cents2dollarsAndCents(getCost()));
+        //return the whole purchase onto the reciept 
+        return output;
+    }
+
+    /**
+     * 
+     * @return the cost of the candy
+     */
+    @Override
+    public int getCost() {
+        //calculate the total cost of the candy
+        double cost = this.weight*this.pricePerLbs;
+        //round the cost 
+        cost = Math.round(cost);
+        //return the price of the candy in the form of an integer
+        return (int) cost;
     }
 }
--- src/Sundae.java
+++ src/Sundae.java
@@ -9,22 +9,54 @@
  * 
  * Remember, we can use IceCream to do some of the heavy lifting for us!
  * 
- * @author YOURNAMEHERE
+ * @author Ali Murra
  */
-public class Sundae{
+public class Sundae extends IceCream{
     
-
+    //declare variables 
+    private String toppingName;
+    private int toppingCost;
     
+    /**
+     * 
+     * @param icName name of the ice cream
+     * @param icCost cost of the ice cream
+     * @param toppingName name of the topping
+     * @param toppingCost cost of the topping
+     */
     public Sundae(String icName, int icCost, String toppingName, int toppingCost)
     {
-       
+       //send the name of the sundae to IceCream
+       super(icName, icCost);
+       //make the variables in this class equal to the ones passed in
+       this.toppingName = toppingName;
+       this.toppingCost = toppingCost;
     }
     
-
-    
+    /**
+     * 
+     * @return the name and cost of the sundae and topping
+     */
     public String toString()
     {
-        return "";
+        //output the name of the topping, ice cream and costs
+        String output = this.toppingName + " Sundae with " + "\n";
+        output += this.getName();
+        //format to line up the price
+        int widthPreCost = DessertShoppe.RECEIPT_WIDTH - this.getName().length();
+        output += String.format("%" + widthPreCost + "s%n", DessertShoppe.cents2dollarsAndCents(getCost()));
+        //return the whole purchase onto the reciept 
+        return output;
     }
     
+    /**
+     * 
+     * @return the total cost of the sundae
+     */
+    @Override
+    public int getCost() {
+        //the total cost is the cost of the ice cream and the topping
+        int cost = this.toppingCost + super.getCost();
+        return cost;
+    }
 }
